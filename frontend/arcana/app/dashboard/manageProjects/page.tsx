88d885917791bdf66b9116b3acd391e9ef96c28f
aswatson <aswatson95@gmail.com>
2024-01-30 01:59:50 -0700

    projects page filled in

--- frontend/arcana/app/dashboard/manageProjects/page.tsx
+++ frontend/arcana/app/dashboard/manageProjects/page.tsx
@@ -16,14 +16,40 @@ const ManageProjectsPage = () => {
   const [dateEnd, setDateEnd] = useState('');
   const [initials, setInitials] = useState('');
 
-  const [showCreateProjectDialog, setShowDialog] = useState(false);
+  const [showCreateProjectDialog, setShowCreateProjectDialog] = useState(false);
+  const [showIngestLogDialog, setShowIngestLogDialog] = useState(false);
+  const [showDeleteProjectDialog, setShowDeleteProjectDialog] = useState(false);
 
-  const handleOpenDialog = () => {
-    setShowDialog(true);
+  const handleOpenDialog = (dialogType: string) => {
+    switch (dialogType) {
+      case 'createProject':
+        setShowCreateProjectDialog(true);
+        break;
+      case 'ingestLog':
+        setShowIngestLogDialog(true);
+        break;
+      case 'deleteProject':
+        setShowDeleteProjectDialog(true);
+        break;
+      default:
+        break;
+    }
   };
 
-  const handleCloseDialog = () => {
-    setShowDialog(false);
+  const handleCloseDialog = (dialogType: string) => {
+    switch (dialogType) {
+      case 'createProject':
+        setShowCreateProjectDialog(false);
+        break;
+      case 'ingestLog':
+        setShowIngestLogDialog(false);
+        break;
+      case 'deleteProject':
+        setShowDeleteProjectDialog(false);
+        break;
+      default:
+        break;
+    }
   };
 
   return (
@@ -32,13 +58,23 @@ const ManageProjectsPage = () => {
       <NavBar />
       <div className="flex min-h-screen flex-col items-center justify-between p-24">
         <Container
-          className="d-flex justify-content-center align-items-center position-absolute top-50 start-50 translate-middle rounded bg-light"
+          className="d-flex flex-column justify-content-between"
           style={{ width: "800px", height: "500px" }}>
-          <h1>Manage Projects</h1>
-          <Button variant="primary" onClick={handleOpenDialog}>+ Create Project</Button>
+          <div>
+            <Container className="d-flex justify-content-between align-items-center">
+              <h1>Manage Projects</h1>
+              <Button variant="primary" onClick={() => handleOpenDialog('createProject')}>+ Create Project</Button>
+            </Container>
+          </div>
+          <div className="mt-2">
+            <Button variant="primary" onClick={() => handleOpenDialog('ingestLog')}>Ingest Log</Button>
+            <Button variant="primary" onClick={() => handleOpenDialog('deleteProject')}>Delete Project</Button>
+            <Button variant="primary">Open Project</Button>
+          </div>
+        
 
           {/* Create Project Dialog */}
-          <Modal show={showCreateProjectDialog} onHide={handleCloseDialog}>
+          <Modal show={showCreateProjectDialog} onHide={() => handleCloseDialog('createProject')}>
             <Modal.Header closeButton>
               <Modal.Title>Create Project</Modal.Title>
             </Modal.Header>
@@ -88,10 +124,53 @@ const ManageProjectsPage = () => {
               </div>
             </Modal.Body>
             <Modal.Footer>
-              <Button variant="secondary" onClick={handleCloseDialog}>Cancel</Button>
-              <Button variant="primary" onClick={handleCloseDialog}>Create Project</Button>
+              <Button variant="secondary" onClick={() => handleCloseDialog('createProject')}>Cancel</Button>
+              <Button variant="primary" onClick={() => handleCloseDialog('createProject')}>Create Project</Button>
             </Modal.Footer>
           </Modal>
+
+          {/* Ingest Log Dialog */}
+          <Modal show={showIngestLogDialog} onHide={() => handleCloseDialog('ingestLog')}>
+            <Modal.Header closeButton>
+              <Modal.Title>Ingest Logs</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <div>
+                <p>Select a directory to ingest logs from.</p>
+              </div>
+              <div>
+                <p>Log Directory</p>
+              </div>
+              <div className="mb-3 d-flex">
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="ex. /Location/folder"
+                  value={projectLocation}
+                  onChange={(e) => setProjectLocation(e.target.value)}
+                />
+                <Button variant="primary">Browse</Button>
+              </div>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={() => handleCloseDialog('ingestLog')}>Cancel</Button>
+              <Button variant="primary" onClick={() => handleCloseDialog('ingestLog')}>Ingest Logs</Button>
+            </Modal.Footer>
+          </Modal>
+
+          {/* Delete Project Dialog */}
+          <Modal show={showDeleteProjectDialog} onHide={() => handleCloseDialog('deleteProject')}>
+            <Modal.Header closeButton>
+            </Modal.Header>
+            <Modal.Body>
+              <h1>Are you sure you want to delete ?</h1>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="secondary" onClick={() => handleCloseDialog('deleteProject')}>Cancel</Button>
+              <Button variant="primary" onClick={() => handleCloseDialog('deleteProject')}>Delete</Button>
+            </Modal.Footer>
+          </Modal>
+
         </Container>
       </div>
       <Footer />
