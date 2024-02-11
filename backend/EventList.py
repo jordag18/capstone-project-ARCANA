eb2167228f0f8b372143b6e1fb8af35416f8af7e
Samanthavsilva <samanthavsilva2001@gmail.com>
2024-02-11 13:13:58 -0700

    Updated Log Ingestion classes

---
+++ backend/EventList.py
@@ -0,0 +1,8 @@
+class EventList:
+    def __init__(self):
+        # Initialize an empty list of EventRepresenters
+        self.events = []
+
+    def addEvent(self, event):
+        # Add the given EventRepresenter to the list
+        self.events.append(event)
