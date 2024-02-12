9c601c05b0b96b3d867fb91bd94b4097dec5006e
lafierro4 <lafierro4@miners.utep.edu>
2024-02-12 16:37:10 -0700

    Added file traveral

--- backend/FileHandler.py
+++ backend/FileHandler.py
@@ -6,6 +6,28 @@ class FileHandler:
         self.logDirPath = os.path.join("..","pdrr")
         self.logDir = open(self.logDirPath,'r+')
 
+    def get_log_files(root_dir):
+        log_files = []
+    
+        for subdir, dirs, files in os.walk(root_dir):
+            print(subdir,dirs,files)
+            for file in files:
+                if file.endswith(".csv"):
+                    log_files.append(os.path.join(subdir, file))
+            if "red" in dirs:
+                red_dir = os.path.join(subdir, "red")
+                for red_subdir, red_dirs, red_files in os.walk(red_dir):
+                    for red_file in red_files:
+                        if red_file.endswith(".csv"):
+                            log_files.append(os.path.join(red_subdir, red_file))
+            if "white" in dirs:
+                white_dir = os.path.join(subdir, "white")
+                for white_file in os.listdir(white_dir):
+                    if white_file.endswith(".csv"):
+                        log_files.append(os.path.join(white_dir, white_file))
+
+        return log_files
+
     def readCSV(self, file_name):
         try:
             with open(file_name, 'r', newline='') as csv_file:
