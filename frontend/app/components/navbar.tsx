d1d36a228a3e94f130e9d2e6a4b0c6f8c44efe3e
aswatson <aswatson95@gmail.com>
2024-02-12 22:56:28 -0700

    deleted arcana text logo

--- frontend/app/components/navbar.tsx
+++ frontend/app/components/navbar.tsx
@@ -8,7 +8,7 @@ import React from "react";
 import DacLogo from "../ui/dacLogo";
 import ActiveAnalyst from "../ui/activeAnalyst";
 import IconLink from "../ui/iconLink";
-import ArcanaTextLogo from "../ui/arcanaTextLogo";
+import Link from "next/link";
 
 import { Diagram3 } from "react-bootstrap-icons";
 import { UiRadios } from "react-bootstrap-icons";
@@ -22,41 +22,45 @@ export function NavBar() {
     <>
       <Navbar collapseOnSelect className="bg-body-tertiary align-middle custom-navbar">
         <DacLogo />
+        <Navbar.Brand className="mr-auto p-6" style={{ fontSize: "24px", textDecoration: "underline" }}>
+          <Link id="link" href="/" className="text-decoration-none text-reset">
+              <strong>ARCANA</strong>
+          </Link>
+        </Navbar.Brand>
         <Navbar.Toggle
         aria-controls="responsive-navbar-nav"
         className="justify-center"
-      />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="me-auto mb-auto">
-          <ArcanaTextLogo />
-          <IconLink
-            icon={<HouseDoorFill />}
-            linkName="Home"
-            link="/"
-          />
-          <IconLink
-            icon={<Folder2 />}
-            linkName="Manage Projects"
-            link="/dashboard/projectsMenu"
-          />
-          <IconLink
-            icon={<CloudArrowUp />}
-            linkName="Sync Projects"
-            link="/dashboard/syncMenu"
-          />
-          <IconLink
-            icon={<UiRadios />}
-            linkName="Manage Events"
-            link="/dashboard/eventMenu"
-          />
-          <IconLink
-            icon={<Diagram3 />}
-            linkName="Manage Event Graphs"
-            link="/dashboard/eventGraphMenu"
-          />
-        </Nav>
-      </Navbar.Collapse>
-      <Navbar.Toggle />
+        />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto mb-auto">
+            <IconLink
+              icon={<HouseDoorFill />}
+              linkName="Home"
+              link="/"
+            />
+            <IconLink
+              icon={<Folder2 />}
+              linkName="Manage Projects"
+              link="/dashboard/projectsMenu"
+            />
+            <IconLink
+              icon={<CloudArrowUp />}
+              linkName="Sync Projects"
+              link="/dashboard/syncMenu"
+            />
+            <IconLink
+              icon={<UiRadios />}
+              linkName="Manage Events"
+              link="/dashboard/eventMenu"
+            />
+            <IconLink
+              icon={<Diagram3 />}
+              linkName="Manage Event Graphs"
+              link="/dashboard/eventGraphMenu"
+            />
+          </Nav>
+        </Navbar.Collapse>
+        <Navbar.Toggle />
         <ActiveAnalyst />
       </Navbar>
     </>
