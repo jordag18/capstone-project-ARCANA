d26d84a77b39b7699721f84edf286e0ab03346c5
aswatson <aswatson95@gmail.com>
2024-02-12 23:08:27 -0700

    removed dac logo

--- frontend/app/components/navbar.tsx
+++ frontend/app/components/navbar.tsx
@@ -2,34 +2,33 @@
  * Renders the navigation bar component.
  * @returns The rendered navigation bar component.
  */
+import { Diagram3, UiRadios, Folder2, CloudArrowUp, HouseDoorFill } from "react-bootstrap-icons";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import React from "react";
-import DacLogo from "../ui/dacLogo";
 import ActiveAnalyst from "../ui/activeAnalyst";
 import IconLink from "../ui/iconLink";
-import Link from "next/link";
-
-import { Diagram3 } from "react-bootstrap-icons";
-import { UiRadios } from "react-bootstrap-icons";
-import { Folder2 } from "react-bootstrap-icons";
-import { CloudArrowUp } from "react-bootstrap-icons";
-import { HouseDoorFill } from "react-bootstrap-icons";
 
 
 export function NavBar() {
   return (
     <>
       <Navbar collapseOnSelect className="bg-body-tertiary align-middle custom-navbar">
-        <DacLogo />
-        <Navbar.Brand className="mr-auto p-6" style={{ fontSize: "24px", textDecoration: "underline" }}>
-          <Link id="link" href="/" className="text-decoration-none text-reset">
-              <strong>ARCANA</strong>
-          </Link>
+        <Navbar.Brand 
+          className="mr-auto p-6" 
+          style={{ fontSize: "24px", textDecoration: "underline", color: "black" }}
+          href="/"
+          >
+            <img
+              className="p-1"
+              src="/devcom.png"
+              height={100}
+              width={100}
+            />
+          <strong>ARCANA</strong>
         </Navbar.Brand>
         <Navbar.Toggle
-        aria-controls="responsive-navbar-nav"
-        className="justify-center"
+          aria-controls="responsive-navbar-nav"
+          className="justify-center"
         />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto mb-auto">
