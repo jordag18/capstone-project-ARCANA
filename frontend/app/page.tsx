2f39969eefc85c22d53245c58402685bc388e4c5
aswatson <aswatson95@gmail.com>
2024-02-18 01:42:43 -0700

    app updated with glovals

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,6 +4,7 @@ import { useRouter } from "next/navigation";
 import { Footer } from "./components/footer";
 import StartUpPrompt from "./components/startup-prompt";
 import NavBar from "./components/navbar";
+import './globals.css';
 //import ThemeHandler from "./util/themeHandler";
 
 export default function Home() {
