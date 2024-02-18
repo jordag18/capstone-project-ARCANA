72968d8ae2d46a05743e18121aca0b179a964538
aswatson <aswatson95@gmail.com>
2024-02-18 02:39:05 -0700

    navbar icons aligned

--- frontend/app/components/navbar.tsx
+++ frontend/app/components/navbar.tsx
@@ -20,7 +20,6 @@ export function NavBar () {
           alt={ "logo image of DAC CEAD organization." } />
         <Navbar.Brand
           className="mx-auto mr-auto p-2"
-          style={ { fontSize: "24px", textDecoration: "underline" } }
           href="/"
         >
           <strong>ARCANA</strong>
@@ -32,22 +31,22 @@ export function NavBar () {
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="mx-auto me-auto mb-auto mr-auto justify-center flex-column flex-md-row" style={ { fontSize: "18px" } }>
             <Nav.Link href="/" className="mx-md-2">
-              <HouseDoorFill className="mb-auto" /> <strong>Home</strong>
+              <HouseDoorFill className="mb-0" /> <strong>Home</strong>
             </Nav.Link>
             <Nav.Link href="/dashboard/projectsMenu" className="mx-md-2">
-              <Folder2 className="mb-auto" /> <strong>Manage Projects</strong>
+              <Folder2 className="mb-0" /> <strong>Manage Projects</strong>
             </Nav.Link>
             <Nav.Link href="/dashboard/syncMenu" className="mx-md-2">
-              <CloudArrowUp className="mb-auto" /> <strong>Sync Projects</strong>
+              <CloudArrowUp className="mb-0" /> <strong>Sync Projects</strong>
             </Nav.Link>
             <Nav.Link href="/dashboard/eventMenu" className="mx-md-2">
-              <UiRadios className="mb-auto" /> <strong>Manage Events</strong>
+              <UiRadios className="mb-0" /> <strong>Manage Events</strong>
             </Nav.Link>
             <Nav.Link href="/dashboard/eventGraphMenu" className="mx-md-2">
-              <Diagram3 className="mb-auto" /> <strong>Manage Event Graphs</strong>
+              <Diagram3 className="mb-0" /> <strong>Manage Event Graphs</strong>
             </Nav.Link>
             <Nav.Link href="/dashboard/systemMenu" className="mx-md-2">
-              <GearWideConnected className="mb-auto" /> <strong>Manage System Settings</strong>
+              <GearWideConnected className="mb-0" /> <strong>Manage System Settings</strong>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
