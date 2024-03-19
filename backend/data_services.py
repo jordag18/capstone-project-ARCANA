df914e6dc8ed95bf366d25c7b19f8595d567424d
lafierro4 <lfierr21@gmail.com>
2024-03-18 21:56:10 -0600

    modified event_representer & data_services, made file of event representer strs

--- backend/data_services.py
+++ backend/data_services.py
@@ -7,16 +7,13 @@ from event_representer import EventRepresenter
 def create_event_representer(initials, team, vector_id, description, data_source, icon, action_title=None, last_modified=None,
                              source_host=None, target_host_list=None, location=None, posture=None, timestamp=None,
                              is_malformed=False):
+    print("create event")
     event = EventRepresenter(
-        initials=initials,
-        team=team,
-        vector_id=vector_id,
-        description=description,
-        data_source=data_source,
-        icon=icon,
+        initials= initials,team= team,
+        vector_id=vector_id, description=description,
+        data_source=data_source, icon=icon,
         action_title=action_title,
         last_modified=last_modified if last_modified is not None else datetime.datetime.now,
-
         source_host=source_host if source_host is not None else "",
         target_host_list=target_host_list if target_host_list else [],
         location=location if location is not None else "",
@@ -24,6 +21,6 @@ def create_event_representer(initials, team, vector_id, description, data_source
         timestamp=timestamp if timestamp is not None else datetime.datetime.now,
         is_malformed=is_malformed
     )
-
-    event.save()
+    print("exit event rep")
+    #event.save()
     return event
