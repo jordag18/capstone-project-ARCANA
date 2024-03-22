6854b091fb086cdbb1a04a3b6baf6425755ee2a3
dlcastaneda <dlcastaneda@miners.utep.edu>
2024-03-21 22:16:18 -0600

    EventsManager to perform CRUD operations using JSON

--- backend/events_manager.py
+++ backend/events_manager.py
@@ -1,5 +1,7 @@
+#Diana Castaneda
 import event_representer as event_representer
 from event_list import EventList
+import json
 
 class EventsManager:
     '''def __init__(self):
@@ -12,3 +14,37 @@ class EventsManager:
     def load_events(self):
         # Assuming you want to pull all documents from the EventRepresenter collection
         return list(event_representer.objects.all())
+    def save_events(self, filename):
+        '''save events to a JSON file'''
+        with open(filename, 'w') as file:
+            json.dump(self.events, file, indent=4)
+
+    def create_event(self, event_data):
+        '''Create a new event'''
+        self.events.append(event_data)
+    
+    def update_event(self, event_id, updated_data):
+        '''update an existing event'''
+        for event in self.events:
+            if event['id'] == event_id:
+                event.update(updated_data)
+
+    def delete_event(self, event_id):
+        '''delete an event by id'''
+        self.events = [event for event in self.events if event['id'] != event_id]
+    
+    def get_event(self, event_id):
+        '''retrieve an event by id'''
+        for event in self.events:
+            if event['id']==event_id:
+                return event
+            return None
+    
+    def get_all_events(self):
+        '''retrieve all events'''
+        return self.events
+    
+
+
+
+
