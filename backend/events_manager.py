84b37e19ad2a9103b059a79a13a8adf5a1a64fdf
dlcastaneda <dlcastaneda@miners.utep.edu>
2024-04-01 19:15:01 -0600

    EventsManager edit_event added/ remove some unused code

--- backend/events_manager.py
+++ backend/events_manager.py
@@ -1,47 +1,60 @@
-#Diana Castaneda CRUD
 import event_representer as event_representer
 from event_list import EventList
 import json
 
 class EventsManager:
-    '''def __init__(self):
-        # Initialize EventList
-        self.eventList = EventList()
-'''
+
     def __init__(self):
         self.event_representer_list = EventList()
 
     def load_events(self):
         # Assuming you want to pull all documents from the EventRepresenter collection
         return list(event_representer.objects.all())
+    
     def save_events(self, filename):
-        '''save events to a JSON file'''
+        #save events to a JSON file
         with open(filename, 'w') as file:
             json.dump(self.events, file, indent=4)
 
     def create_event(self, event_data):
-        '''Create a new event'''
-        self.events.append(event_data)
+        #Create a new event
+        events = self.load_events() #load existing data
+        events.append(event_data) #new event
+        self.save_events(events)
     
     def update_event(self, event_id, updated_data):
-        '''update an existing event'''
-        for event in self.events:
+        #update an existing event
+        events = self.load_events()
+        for event in events:
             if event['id'] == event_id:
                 event.update(updated_data)
+                break
+            self.save_events(events) #save udpdated events
 
     def delete_event(self, event_id):
-        '''delete an event by id'''
-        self.events = [event for event in self.events if event['id'] != event_id]
+        #delete an event by id
+        events = self.load_events()
+        events = [event for event in events if event['id']] #filter out event to be deleted
+        self.save_events(events)
+
+    def edit_event(self, event_id, updated_data):
+        events = self.load_events()
+        for event in events:
+            if event['id'] == event_id:
+                event.update(updated_data)
+                break
+            self.save_events(events) #save updated events
     
     def get_event(self, event_id):
-        '''retrieve an event by id'''
-        for event in self.events:
+        #retrieve an event by id
+        events = self.load_events()
+        for event in events:
             if event['id']==event_id:
                 return event
             return None
     
     def get_all_events(self):
-        '''retrieve all events'''
+        #retrieve all events
         return self.events
     
 
