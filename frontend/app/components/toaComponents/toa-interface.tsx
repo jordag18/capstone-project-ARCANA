c2f33b3b53b9cc6dd71de59fed8512555d805ef2
aswatson <aswatson95@gmail.com>
2024-04-19 22:14:31 -0600

    defined the undefined

--- frontend/app/components/toaComponents/toa-interface.tsx
+++ frontend/app/components/toaComponents/toa-interface.tsx
@@ -1,16 +1,16 @@
 export interface CreateToa {
     team: string;
     actionTitle: string;
-    imageName: string | null;
+    imageName: string;
 }
 
 export interface EditToa{
     team: string;
     actionTitle: string;
-    imageName: string | null;
+    imageName: string;
     isDefault: boolean;
     oldTeam: string; 
     oldActionTitle: string; 
-    oldImageName: string | null; 
-    oldIsDefault: boolean | null; 
+    oldImageName: string; 
+    oldIsDefault: boolean; 
 }
