080196bb7d12f8333ca91d039a48c28ca3f4cd07
aswatson <aswatson95@gmail.com>
2024-04-22 00:32:34 -0600

    create event function not working, possibly from object mapping

--- frontend/app/components/eventComponents/event-create-modal.tsx
+++ frontend/app/components/eventComponents/event-create-modal.tsx
@@ -1,11 +1,23 @@
 import { useState, useEffect } from 'react'
 import { useProject } from '@/app/contexts/ProjectContext'
 import { CreateEvent } from './event-interface'
+import axios from 'axios'
 
 interface createEventProp {
     newEvent: CreateEvent
     isModalOpen: boolean
     onClose: () => void
+}
+
+interface IconInfo {
+  image: string;
+  isDefault: boolean;
+}
+
+interface IconLibrary {
+  [team: string]: {
+      [iconName: string]: IconInfo;
+  };
 }
 
 const CreateEventModal: React.FC<createEventProp> = ({
@@ -15,45 +27,50 @@ const CreateEventModal: React.FC<createEventProp> = ({
 }) => {
     const { project } = useProject()
     const [formData, setFormData] = useState<CreateEvent>(newEvent)
+    const [iconLibraries, setIconLibraries] = useState<IconLibrary>({})
+    const [autoEdge, setAutoEdge] = useState(false)
+    const [selectedIcon, setSelectedIcon] = useState(null)
 
-    const handleSubmit = async () => {
-    console.log("Create Event", formData, "body", JSON.stringify(formData));
-
-    try {
-        const response = await fetch(`http://localhost:8000/api/createEvent/${project.name}`, {
-            method: "PATCH",
-            headers: {
-                "Content-type": "application/json"
-            },
-            body: JSON.stringify(formData)
-        });
-
-        if (!response.ok) {
-            const errorData = await response.json();
-            console.error('Failed to create event:', errorData);
-            alert(`Failed to create event: ${errorData.detail || response.statusText}`);
-            return;
-        }
-
-        const data = await response.json();
-        console.log('Event created successfully:', data);
-        onClose();
-    } catch (error) {
-        console.error("Error creating event: ", error);
-        alert("Error creating event: " + error.message);
-    }
-};
-
-
-    const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-      const file = e.target.files?.[0];
-      if (file) {
-          setFormData((prevFormData) => ({
-              ...prevFormData,
-              icon: file.name
-          }));
+    const fetchIconLibrary = async () => {
+      try {
+          const response = await axios.get(`http://localhost:8000/api/project/${project.id}/icon-libraries`);
+          setIconLibraries(response.data);
+      } catch (error) {
+          console.error('Failed to fetch icon libraries:', error);
       }
   };
+
+    const handleSubmit = async () => {
+      const formDataJson = JSON.stringify(formData);
+      const autoEdgesJson = JSON.stringify({ auto_edge: autoEdge})
+
+      try {
+          const response = await fetch(`http://localhost:8000/api/createEvent/${project.name}`, {
+              method: "PATCH",
+              headers: {
+                  "Content-type": "application/json"
+              },
+              body: JSON.stringify({
+                event_create: formData,
+                auto_edges: { auto_edge: autoEdge }
+              })
+          });
+
+          if (!response.ok) {
+              const errorData = await response.json();
+              console.error('Failed to create event:', errorData);
+              alert(`Failed to create event: ${errorData.detail || response.statusText}`);
+              return;
+          }
+
+          const data = await response.json();
+          console.log('Event created successfully:', data);
+          onClose();
+      } catch (error) {
+          console.error("Error creating event: ", error);
+          alert("Error creating event: " + error.message);
+      }
+    };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -70,7 +87,17 @@ const CreateEventModal: React.FC<createEventProp> = ({
             [name]: name === "icon" ? value.replace(/^.*[\\\/]/, '') : value
         }));
     }
-};
+  };
+
+  const handleIconChange = (iconName, iconInfo, team) => {
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      action_title: iconName,
+      icon: `/Icons/${iconInfo.image}`,
+      team: team
+    }))
+    setSelectedIcon(iconName)
+  }
     
 
     useEffect(() => {
@@ -82,8 +109,12 @@ const CreateEventModal: React.FC<createEventProp> = ({
         }
     }, [isModalOpen]);
 
+    useEffect(() => {
+      fetchIconLibrary();
+    }, [project.id])
+
     return (
-      <dialog id='create_event_modal' className='modal' style={{ width: '80%', height: '80%' }}>
+      <dialog id='create_event_modal' className='modal' style={{ width: '100%', height: '80%' }}>
         <div className='modal-box'>
             <form method='dialog'>
                 <button className='btn btn-sm btn-circle btn-ghost absolute right-2 top-2' onClick={onClose}>
@@ -91,167 +122,158 @@ const CreateEventModal: React.FC<createEventProp> = ({
                 </button>
             </form>
             <div className="flex flex-col space-y-2">
-          <h3 className="font-bold text-lg">Create Event</h3>
-          <div className="flex flex-row space-x-2">
-            <div className="flex-1 flex-col">
-              <div className="flex-col">
-                <h2>Action Title</h2>
-                <label className="input input-bordered flex items-center gap-2">
-                  <input
-                    type="text"
-                    name="action_title"
-                    className="grow"
-                    placeholder="Action Title"
-                    value={formData?.action_title}
-                    onChange={handleChange}
-                  />
-                </label>
-              </div>
-              <div className="flex-col">
-                <h2>Initials</h2>
-                <label className="input input-bordered flex items-center gap-2">
-                  <input
-                    type="text"
-                    name="initials"
-                    className="grow"
-                    placeholder="Initials"
-                    value={formData?.initials}
-                    onChange={handleChange}
-                  />
-                </label>
-              </div>
-              <div className="flex-col">
-                <h2>Team</h2>
-                <label className="input input-bordered flex items-center gap-2">
-                  <input
-                    type="text"
-                    name="team"
-                    className="grow"
-                    value={formData?.team}
-                    onChange={handleChange}
-                  />
-                </label>
-              </div>
-              <div className="flex-col">
-                <h2>Vector ID</h2>
-                <label className="input input-bordered flex items-center gap-2">
-                  <input
-                    type="text"
-                    name="vector_id"
-                    className="grow"
-                    placeholder="Vector ID"
-                    value={formData?.vector_id}
-                    onChange={handleChange}
-                  />
-                </label>
-              </div>
-              <div className="flex-col">
-                <h2>Location</h2>
-                <label className="input input-bordered flex items-center gap-2">
-                  <input
-                    type="text"
-                    name="location"
-                    className="grow"
-                    value={formData?.location}
-                    onChange={handleChange}
-                  />
-                </label>
-              </div>
-            </div>
-            <div className="flex-1 flex-col">
-              <div className="flex-col">
-                <h2>Source Host</h2>
-                <label className="input input-bordered flex items-center gap-2">
-                  <input
-                    type="text"
-                    name="source_host"
-                    className="grow"
-                    value={formData?.source_host}
-                    onChange={handleChange}
-                  />
-                </label>
-              </div>
-              <div className="flex-col">
-                <h2>Target Host</h2>
-                <label className="input input-bordered flex items-center gap-2">
-                  <input
-                    type="text"
-                    name="target_host_list"
-                    className="grow"
-                    value={formData?.target_host_list}
-                    onChange={handleChange}
-                  />
-                </label>
-              </div>
-              <div className="flex-col">
-                <h2>Data Source</h2>
-                <label className="input input-bordered flex items-center gap-2">
-                  <input
-                    type="text"
-                    name="data_source"
-                    className="grow"
-                    placeholder="Data Source"
-                    value={formData?.data_source}
-                    onChange={handleChange}
-                  />
-                </label>
-              </div>
-              <div className="flex-col">
-                <h2>Posture</h2>
-                <label className="input input-bordered flex items-center gap-2">
-                  <input
-                    type="text"
-                    name="posture"
-                    className="grow"
-                    value={formData?.posture}
-                    onChange={handleChange}
-                  />
-                </label>
-              </div>
-              <div className="flex-col">
-                <h2>Timestamp</h2>
-                <label className="input input-bordered flex items-center gap-2">
-                  <input
-                    type="datetime-local"
-                    name="timestamp"
-                    className="grow"
-                    value={formData?.timestamp}
-                    onChange={handleChange}
-                  />
-                </label>
+              <h3 className="font-bold text-lg">Create Event</h3>
+              <div className="flex flex-row space-x-2">
+                <div className="flex-1 flex-col">
+                  <div className="flex-col">
+                  </div>
+                  <div className="flex-col">
+                    <h2>Initials</h2>
+                    <label className="input input-bordered flex items-center gap-2">
+                      <input
+                        type="text"
+                        name="initials"
+                        className="grow"
+                        placeholder="Initials"
+                        value={formData?.initials}
+                        onChange={handleChange}
+                      />
+                    </label>
+                  </div>
+                  <div className="flex-col">
+                  </div>
+                  <div className="flex-col">
+                    <h2>Vector ID</h2>
+                    <label className="input input-bordered flex items-center gap-2">
+                      <input
+                        type="text"
+                        name="vector_id"
+                        className="grow"
+                        placeholder="Vector ID"
+                        value={formData?.vector_id}
+                        onChange={handleChange}
+                      />
+                    </label>
+                  </div>
+                  <div className="flex-col">
+                    <h2>Location</h2>
+                    <label className="input input-bordered flex items-center gap-2">
+                      <input
+                        type="text"
+                        name="location"
+                        className="grow"
+                        value={formData?.location}
+                        onChange={handleChange}
+                      />
+                    </label>
+                  </div>
+                  <div className="flex-col">
+                    <h2>Posture</h2>
+                    <label className="input input-bordered flex items-center gap-2">
+                      <input
+                        type="text"
+                        name="posture"
+                        className="grow"
+                        value={formData?.posture}
+                        onChange={handleChange}
+                      />
+                    </label>
+                  </div>
+                </div>
+                <div className="flex-1 flex-col">
+                  <div className="flex-col">
+                    <h2>Source Host</h2>
+                    <label className="input input-bordered flex items-center gap-2">
+                      <input
+                        type="text"
+                        name="source_host"
+                        className="grow"
+                        value={formData?.source_host}
+                        onChange={handleChange}
+                      />
+                    </label>
+                  </div>
+                  <div className="flex-col">
+                    <h2>Target Host</h2>
+                    <label className="input input-bordered flex items-center gap-2">
+                      <input
+                        type="text"
+                        name="target_host_list"
+                        className="grow"
+                        value={formData?.target_host_list}
+                        onChange={handleChange}
+                      />
+                    </label>
+                  </div>
+                  <div className="flex-col">
+                    <h2>Data Source</h2>
+                    <label className="input input-bordered flex items-center gap-2">
+                      <input
+                        type="text"
+                        name="data_source"
+                        className="grow"
+                        placeholder="Data Source"
+                        value={formData?.data_source}
+                        onChange={handleChange}
+                      />
+                    </label>
+                  </div>
+                  <div className="flex-col">
+                    <h2>Timestamp</h2>
+                    <label className="input input-bordered flex items-center gap-2">
+                      <input
+                        type="datetime-local"
+                        name="timestamp"
+                        className="grow"
+                        value={formData?.timestamp}
+                        onChange={handleChange}
+                      />
+                    </label>
+                  </div>
+                </div>
               </div>
               <h2>Description</h2>
-                <label className="input input-bordered flex items-center gap-2">
-                  <input
-                    type="text"
-                    name="description"
-                    className="grow"
-                    placeholder="Description"
-                    value={formData?.description}
-                    onChange={handleChange}
-                  />
-                </label>
-              </div>
-              <h2>Icon</h2>
-                <label className="input input-bordered flex items-center gap-2">
-                    <input
-                        type="file"
-                        name="icon"
-                        accept="image/*"
+                    <label className="input input-bordered flex items-center gap-2">
+                      <input
+                        type="text"
+                        name="description"
                         className="grow"
-                        onChange={handleFileChange}
-                    />
-                </label>
-              <div className="flex-col">
+                        placeholder="Description"
+                        value={formData?.description}
+                        onChange={handleChange}
+                      />
+                    </label>
+              <h2>Icon</h2>
+              <div style={{overflow: 'auto', height: '10rem', border: 'solid'}}>
+                {Object.entries(iconLibraries).map(([team, icons]) => (
+                    <div key={team}>
+                        <h2 style={{marginLeft: '1rem'}}>{team} Team TOA Icons</h2>
+                        <hr style={{height: '2px', width: '100%', color: 'black', backgroundColor: 'black'}}/>
+                        <div style={{display: 'flex', alignItems: 'center'}}>
+                            {Object.entries(icons).map(([iconName, iconInfo]) => (
+                                <div className={`hover:bg-gray-200 ${selectedIcon === iconName ? 'bg-yellow-200' : ''}`} key={iconName} style={{marginLeft: '1rem', marginRight: '2rem', marginBottom: '1rem'}} onClick={() => handleIconChange(iconName, iconInfo, team)}>
+                                    <img src={`/Icons/${iconInfo.image}`} alt={iconName} style={{ width: '50px', height: '50px'}}/>
+                                    <p>{iconName}</p>
+                                    {iconInfo.isDefault && <p style={{ color: 'gray', fontSize: '12px'}}> default</p>}
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <label className="input input-bordered flex items-center gap-2">
+              <input
+                type="checkbox"
+                checked={autoEdge}
+                onChange={(e) => setAutoEdge(e.target.checked)}
+              />
+              Auto Edge
+            </label>
+              <div>
+                <button className="btn bg-gray-300 shadow-md hover:bg-gray-200 ml-2" onClick={handleSubmit}>Create</button>
+              </div>
             </div>
           </div>
-            <div>
-                <button onClick={handleSubmit}>
-                    Create Event
-                </button>
-            </div>
-            </div>
-            </div>
         </dialog>
     );
 };
--- frontend/app/components/eventComponents/event-interface.tsx
+++ frontend/app/components/eventComponents/event-interface.tsx
@@ -18,6 +18,7 @@ export interface Event {
 
 export interface CreateEvent {
   action_title: string;
+  auto_edge: boolean;
   data_source: string;
   description: string;
   icon: string;
