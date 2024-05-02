87029324a4a76d83613745ba3586b83ea8d1a055
aswatson <aswatson95@gmail.com>
2024-05-01 22:42:21 -0700

    deleted unused imports

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,3 @@
-import React from "react";
-import { useRouter } from "next/navigation";
-import { Footer } from "./components/footer";
-import StartUpPrompt from "./components/startup-prompt";
-import NavBar from "./components/navbar";
-import Link from "next/link";
-
 export default function Home() {
   return (
     <main>
