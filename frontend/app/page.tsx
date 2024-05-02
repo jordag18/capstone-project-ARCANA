0cb79b001866a175fea02932aac453125904551c
aswatson <aswatson95@gmail.com>
2024-05-01 23:17:43 -0700

    page updated with submit and change

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,4 +1,20 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import ProjectMenu from "./components/projectComponents/ProjectMenu";
+
 export default function Home() {
+
+  const [initialData, setInitialData] = useState('');
+  const navigate = useNavigate();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInitialData(e.target.value);
+  }
+
+  const handleSubmit = () => {
+    navigate('/dashboard/projectsMenu', { state: {initials: initialData}})
+  }
+
   return (
     <main>
       <h1 style={{fontSize: '2rem', fontWeight: 'bold', textDecoration: 'underline'}}>Welcome to ARCANA</h1>
@@ -10,6 +26,8 @@ export default function Home() {
             name="initials"
             className="grow"
             placeholder="AA"
+            value={initialData}
+            onChange={handleChange}
           />
         </label>
         <button style={{marginTop: '1rem'}} className="btn bg-blue-500 text-white shadow-md hover:bg-blue-600">Submit</button>
