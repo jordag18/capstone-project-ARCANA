d017cd97e5f541f03c920fdecf87dde04ba37052
aswatson <aswatson95@gmail.com>
2024-05-03 11:22:57 -0600

    fixed project page look

--- frontend/app/dashboard/projectsMenu/page.tsx
+++ frontend/app/dashboard/projectsMenu/page.tsx
@@ -38,7 +38,7 @@ const ManageProjectsPage = () => {
               d="M2.25 12.75V12A2.25 2.25 0 0 1 4.5 9.75h15A2.25 2.25 0 0 1 21.75 12v.75m-8.69-6.44-2.12-2.12a1.5 1.5 0 0 0-1.061-.44H4.5A2.25 2.25 0 0 0 2.25 6v12a2.25 2.25 0 0 0 2.25 2.25h15A2.25 2.25 0 0 0 21.75 18V9a2.25 2.25 0 0 0-2.25-2.25h-5.379a1.5 1.5 0 0 1-1.06-.44Z"
             />
           </svg>
-          <h1 className="text-3xl font-semibold">Manage Projects</h1>
+          <h1 className="text-3xl font-semibold" style={{marginRight: '3rem'}}>Manage Projects</h1>
         </div>
         <div className="flex space-x-3 items-center">
           <button
